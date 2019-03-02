c9a4b175f9b4a4ce5592389d419b534277c9a549
Grigor Sargsyan <sargr86@gmail.com>
2019-03-02 11:40:39 +0400

    Translation fixes

--- routes/locations.js
+++ routes/locations.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const validateProvinceData = require('../validators/validateProvinceData');
 
 router.get('/get',locationsController.get);
-router.post('/add',uploadFlag,validateProvinceData.rules,locationsController.add);
+router.post('/add',uploadFlag,locationsController.add);
 router.get('/get-by-name', locationsController.getByName);
-router.put('/update',uploadFlag,validateProvinceData.rules, locationsController.update);
+router.put('/update',uploadFlag, locationsController.update);
 router.delete('/remove', locationsController.remove);
 
 module.exports = router;
