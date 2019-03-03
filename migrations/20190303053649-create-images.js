0daaf2c09dac37323f7e440a96187377664155ad
Grigor Sargsyan <sargr86@gmail.com>
2019-03-03 12:58:44 +0400

    Added images model/migration/service, images adding when new story created

---
+++ migrations/20190303053649-create-images.js
@@ -0,0 +1,57 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      country_id: {
+        type: Sequelize.INTEGER
+      },
+      province_id: {
+        type: Sequelize.INTEGER
+      },
+      direction_id: {
+        type: Sequelize.INTEGER
+      },
+      location_id: {
+        type: Sequelize.INTEGER
+      },
+      story_id: {
+        type: Sequelize.INTEGER
+      },
+      cover: {
+        type: Sequelize.INTEGER
+      },
+      fav: {
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description_en: {
+        type: Sequelize.TEXT
+      },
+      description_ru: {
+        type: Sequelize.TEXT
+      },
+      description_hy: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('images');
+  }
+};
